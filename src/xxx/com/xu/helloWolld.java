a65da03f3f53bb98af72f8651eea91c23858cfc0
xb <2263866012@qq.com>
2021-08-09 11:22:29 +0800

    ho commit3

--- src/xxx/com/xu/helloWolld.java
+++ src/xxx/com/xu/helloWolld.java
@@ -2,11 +2,7 @@ package xxx.com.xu;
 
 public class helloWolld {
     public static void main(String[] args) {
-        System.out.println("helloworld");
-        System.out.println("helloworld2");
-        System.out.println("helloworld3");
-        System.out.println("helloworld4");
-        System.out.println("helloworld5");
-        System.out.println("helloworld666666");
+
+        System.out.println("xubiao ");
     }
 }
